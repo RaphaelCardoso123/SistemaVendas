031209a3cce7b9f4e71ffef91d248134a3569d96
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-01-15 17:32:31 -0300

    ...

---
+++ Repository/VendedorRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SistemaVendas.Context;
+using SistemaVendas.Models;
+
+namespace SistemaVendas.Repository
+{
+    public class VendedorRepository
+    {
+          private readonly VendasContext _context; //O "_"(underline) significa que é global e pode ser acessada em qualquer classe
+          public VendedorRepository(VendasContext context)
+          {
+            _context = context;
+          }
+
+          public void Cadastrar(Vendedor vendedor)
+          {
+            _context.Vendedores.Add(vendedor);
+            _context.SaveChanges();
+          }
+
+    }
+}
