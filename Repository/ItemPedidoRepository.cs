b7ad754184a7628f19d5af5b5168f5a5e4e80d96
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-01-21 18:49:48 -0300

    Finalizando ItemPedidoRepository

--- Repository/ItemPedidoRepository.cs
+++ Repository/ItemPedidoRepository.cs
@@ -8,43 +8,43 @@ namespace SistemaVendas.Repository
     public class ItemPedidoRepository
     {
         private readonly VendasContext _context; 
-          public ItemPedidoRepository(VendasContext context)
-          {
-            _context = context;
-          }
-
-
-        public ItemPedido Cadastrar(ItemPedido itemPedido)
-          {
-            _context.ItensPedido.Add(itemPedido);
-            _context.SaveChanges(); 
-            return itemPedido;
-
-          }
-
-
-          public ItemPedido ObterPorId(int id)
+        public ItemPedidoRepository(VendasContext context)
         {
-            var itemPedido = _context.ItensPedido.Include(x => x.Pedido)
-                                                 .Include(x => x.Servico)
-                                                 .FirstOrDefault(x => x.Id == id);
-            return itemPedido;
+          _context = context;
         }
 
 
-          public void DeletarItemPedido(ItemPedido itemPedido){
-            _context.ItensPedido.Remove(itemPedido);
-            _context.SaveChanges();
+        public ItemPedido Cadastrar(ItemPedido itemPedido)
+        {
+          _context.ItensPedido.Add(itemPedido);
+          _context.SaveChanges(); 
+          return itemPedido;
+        }
+
+
+        public ItemPedido ObterPorId(int id)
+        {
+          var itemPedido = _context.ItensPedido.Include(x => x.Pedido)
+                                                 .Include(x => x.Servico)
+                                                 .FirstOrDefault(x => x.Id == id);
+          return itemPedido;
+        }
+
+
+        public void DeletarItemPedido(ItemPedido itemPedido)
+        {
+          _context.ItensPedido.Remove(itemPedido);
+          _context.SaveChanges();
         }
 
 
         public ItemPedido AtualizarItemPedido(ItemPedido itemPedido)
-                                                              
-          {
-            _context.ItensPedido.Update(itemPedido);
-            _context.SaveChanges(); 
+                                                    
+        {
+          _context.ItensPedido.Update(itemPedido);
+          _context.SaveChanges(); 
 
-            return itemPedido;
-          }
+          return itemPedido;
+        }
     }
 }
